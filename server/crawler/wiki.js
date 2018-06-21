2e7ba226a9c46a782558ff7cb63763223a9285a9
jing <985812572@qq.com>
2018-06-22 02:01:50 +0800

    获取家族详细数据

--- server/crawler/wiki.js
+++ server/crawler/wiki.js
@@ -149,4 +149,65 @@ export const fetchImageFromIMDb = async () => {
   fs.writeFileSync('./completeCharacters.json', JSON.stringify(IMDbCharacters, null, 2), 'utf8')
 }
 
-fetchImageFromIMDb()
+const HOUSES = [
+  {
+    name: 'House Stark of Winterfell',
+    cname: '史塔克家族',
+    words: 'Winter is Coming'
+  },
+  {
+    name: 'House Targaryen',
+    cname: '坦格利安家族',
+    words: 'Fire and Blood'
+  },
+  {
+    name: 'House Lannister of Casterly Rock',
+    cname: '兰尼斯特家族',
+    words: 'Hear Me Roar!'
+  },
+  {
+    name: 'House Arryn of the Eyrie',
+    cname: '艾林家族',
+    words: 'As High as Honor'
+  },
+  {
+    name: 'House Tully of the Riverrun',
+    cname: '徒利家族',
+    words: 'Family, Duty, Honor'
+  },
+  {
+    name: 'House Greyjoy of Pyke',
+    cname: '葛雷乔伊家族',
+    words: 'We Do Not Sow'
+  },
+  {
+    name: "House Baratheon of Storm's End",
+    cname: '风息堡的拜拉席恩家族',
+    words: 'Ours is the Fury'
+  },
+  {
+    name: 'House Tyrell of Highgarden',
+    cname: '提利尔家族',
+    words: 'Growing Strong'
+  },
+  {
+    name: 'House Nymeros Martell of Sunspear',
+    cname: '马泰尔家族',
+    words: 'Unbowed, Unbent, Unbroken'
+  }
+]
+
+// 获取家族详细数据
+export const getHouses = async () => {
+  let data = R.map(getWikiId, HOUSES)
+  data = await Promise.all(data)
+  console.log(data[0])
+  console.log('ID done! 开始获取 detail')
+
+  data = R.map(getWikiDetail, data)
+  data = await Promise.all(data)
+
+  fs.writeFileSync('./wikiHouses.json', JSON.stringify(data, null, 2), 'utf8')
+}
+
+getHouses()
